7ac1f2a1110ade9dda74bd39465c3bf3e039598a
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-03-20 16:05:12 +0200

    get the location value from input
    added a simple input to take the value from there instead of a static
    location value inside service

--- js/app.js
+++ js/app.js
@@ -4,14 +4,16 @@
 
 
   app.controller('LocationCtrl', function($scope, LocationService){
-    LocationService.get().then(function(response){
-      var locationProperty = response.results[0].geometry.location;
-      // console.log(response);
-      LocationService.getBusStops(locationProperty.lat, locationProperty.lng).then(function(response){
-        console.log(response);
-      });
 
+      $scope.findLocation = function() {
+        LocationService.get($scope.locationVal).then(function(response){
+          var locationProperty = response.results[0].geometry.location;
+          // console.log(response);
+          LocationService.getBusStops(locationProperty.lat, locationProperty.lng).then(function(response){
+            console.log(response);
+            console.log($scope.locationVal)
+          });
+        });
+      }
     });
-  })
-
 })();
--- js/locationService.js
+++ js/locationService.js
@@ -4,14 +4,13 @@ angular.module('app.locationservice', [])
     var apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
     var apiKey = 'AIzaSyBAuYHaY4KqFCvKcQg-7cwdPEmgkr_lk5E';
     var components= 'components=administrative_area:London|country:GB'; //made the request london specific
-    var location = 'earls court';
     // uk transport vars
     var transportApiKeyAndId = 'api_key=d9307fd91b0247c607e098d5effedc97&app_id=03bf8009';
     var transportApiUrl = 'http://transportapi.com/v3/uk/bus/stops/near.json?';
 
     return {
 
-      get: function(){
+      get: function(location){
         return $http.get(apiUrl + location + '&' + components + '&key=' + apiKey).then(function(response) {
           return response.data;
         });
