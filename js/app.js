feca2d5b33ddc10e326cefe0abee9c605f372108
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-03-20 15:35:51 +0200

    response from transport api

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 (function(){
   var app = angular.module('app', ['app.locationservice']);
 
+
+
   app.controller('LocationCtrl', function($scope, LocationService){
     LocationService.get().then(function(response){
-      console.log(response);
+      var locationProperty = response.results[0].geometry.location;
+      // console.log(response);
+      LocationService.getBusStops(locationProperty.lat, locationProperty.lng).then(function(response){
+        console.log(response);
+      });
+
     });
   })
 
--- js/locationService.js
+++ js/locationService.js
@@ -4,7 +4,10 @@ angular.module('app.locationservice', [])
     var apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
     var apiKey = 'AIzaSyBAuYHaY4KqFCvKcQg-7cwdPEmgkr_lk5E';
     var components= 'components=administrative_area:London|country:GB'; //made the request london specific
-    var location = 'waterloo';
+    var location = 'earls court';
+    // uk transport vars
+    var transportApiKeyAndId = 'api_key=d9307fd91b0247c607e098d5effedc97&app_id=03bf8009';
+    var transportApiUrl = 'http://transportapi.com/v3/uk/bus/stops/near.json?';
 
     return {
 
@@ -12,8 +15,13 @@ angular.module('app.locationservice', [])
         return $http.get(apiUrl + location + '&' + components + '&key=' + apiKey).then(function(response) {
           return response.data;
         });
-      }
+      },
 
+      getBusStops: function(lat, lon){
+        return $http.get(transportApiUrl + 'lat=' + lat + '&lon=' + lon + '&' + transportApiKeyAndId).then(function(response) {
+          return response.data;
+        });
+      }
     }
 
 
