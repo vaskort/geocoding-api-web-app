1ab2ef86b0f5f71b133d9ae6830ed3093bfe2093
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-03-20 14:44:10 +0200

    london specific request
    Made the request London specific with component filtering,
    https://developers.google.com/maps/documentation/geocoding/intro?csw=1#C
    omponentFiltering

--- js/locationService.js
+++ js/locationService.js
@@ -3,13 +3,14 @@ angular.module('app.locationservice', [])
 
     var apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
     var apiKey = 'AIzaSyBAuYHaY4KqFCvKcQg-7cwdPEmgkr_lk5E';
-    var location = 'Athens';
+    var components= 'components=administrative_area:London|country:GB'; //made the request london specific
+    var location = 'waterloo';
 
     return {
 
       get: function(){
-        return $http.get(apiUrl + location + '&key=' + apiKey).then(function(response) {
-          return response;
+        return $http.get(apiUrl + location + '&' + components + '&key=' + apiKey).then(function(response) {
+          return response.data;
         });
       }
 
