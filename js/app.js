c16b8a952447763f777e92633df81314e56f6e39
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-03-20 22:54:28 +0200

    toggle is working ok, few comments, some styling

--- js/app.js
+++ js/app.js
@@ -3,9 +3,11 @@
 
 
 
-  app.controller('LocationCtrl', function($scope, LocationService){
+  app.controller('LocationCtrl', function($scope, LocationService, uiGmapIsReady, $timeout){
+
     // and now that everything is done show the map
     $scope.mapClass = 'active';
+
     // default value for how to show markers (cluster, spider, none)
     $scope.clusterVal = 'none';
 
@@ -17,18 +19,19 @@
 
     $scope.findLocation = function() {
 
-
       // erase the old markers array
       $scope.markers = [];
+
+      // make the http request
       LocationService.get($scope.locationVal).then(function(response){
         var locationProperty = response.results[0].geometry.location;
-        // console.log(response);
+
+        // make the request to take the lat and long from bus stops
         LocationService.getBusStops(locationProperty.lat, locationProperty.lng).then(function(response){
-
+          // add stops to scope
           $scope.busStationsArray = response.stops;
-          console.log(response);
-          for (var i = 0; i < $scope.busStationsArray.length; i++){
 
+          for (var i = 0; i < $scope.busStationsArray.length; i++){
             $scope.markers.push({
                  id:i,
                  latitude: $scope.busStationsArray[i].latitude,
@@ -37,38 +40,59 @@
                  show:false
              });
           }
-
-
-
         });
       });
     }
 
+    // init options of the map
     $scope.map = {
       center: {
         latitude: 51.532580,
         longitude: -0.135965
       },
-      zoom: 11
+      zoom: 11,
+      refresh: function () {
+        $scope.map.control.refresh(center);
+      }
     };
 
+    // onclick scope function for markers
     $scope.onClick = function(marker, eventName, model) {
         $scope.hideMarkers();
         // and show the clicked one
         model.show = true;
     };
 
-    $scope.hideMarkers = function(model){
+    $scope.hideMarkers = function(){
       // remove the shown markers
       for (var i = 0; i < $scope.markers.length; i++){
         $scope.markers[i].show = false;
       }
-    }
+    };
 
     // hide/show the input container
+    $scope.toggleInput = function(){
+      $scope.toggledClass = !$scope.toggledClass;
+      // we need to refresh the map after the transition of it to make it bigger or else a part of it will be grey
+      $scope.refreshMap();
+    };
+
+    // refreshMap is used when toggling the search box
+    $scope.refreshMap = function(){
+      uiGmapIsReady.promise().then(function (maps) {
+        // timeout is needed because of the css transition
+        $timeout(function() {
+          google.maps.event.trigger(maps[0].map, 'resize');
+        }, 500);
+      });
+    };
+
+
+
 
   });
 
+  // angular google map init http://angular-ui.github.io/angular-google-maps/#!/use
   app.config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
         key: 'AIzaSyAw2pXZURI1WbXDpq99JhyQcr0va44Fyko',
